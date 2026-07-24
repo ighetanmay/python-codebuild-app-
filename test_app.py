5ec2a2ce8e3e7163b035dcf8dbd41d5214389da7
ighetanmay <ighetanmay@gmail.com>
2026-07-24 20:52:31 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,11 +1,9 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello"
-
-@app.route("/health")
-def health():
-    return "Healthy"
+from app import app
+def test_home():
+client = app.test_client()
+response = client.get("/")
+assert response.status_code == 200
+def test_health():
+client = app.test_client()
+response = client.get("/health")
+assert response.status_code == 200
